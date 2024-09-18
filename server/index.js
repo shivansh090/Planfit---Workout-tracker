d603c94e9695a993deaa4f07996a7c8ece61cc74
Shivansh Vikram Singh <116243866+shivansh090@users.noreply.github.com>
2024-09-19 01:07:07 +0530

    Update Cors err

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://planfit-7oge.onrender.com',  // Allow your frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
+  credentials: true, // If sending cookies or tokens
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
