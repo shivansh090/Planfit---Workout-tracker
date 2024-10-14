76470acb28fe0d6bec4cc7f4aaf44202d03594ff
Shivansh Vikram Singh <shivanshvikramsingh764@gmail.com>
2024-10-14 14:07:07 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -7,13 +7,7 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://planfit-7oge.onrender.com',  // Allow your frontend origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
-  credentials: true, // If sending cookies or tokens
-}));
-app.options('*', cors());  // Respond to preflights
-
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
